d242d64113bbcf85317070a48dab4c7b30cf2e5d
m77so <m77code@gmail.com>
2018-06-12 02:56:43 +0900

    cpp init

---
+++ templates/cpp/a.cpp
@@ -0,0 +1,45 @@
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <cstdio>
+#include <string>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <complex>
+#include <vector>
+#include <list>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+using namespace std;
+
+#define y0 y910831
+#define y1 y923456
+#define j0 j771912
+#define j1 j442401
+#define next next18181
+#define prev prev18181
+
+using ll = long long;
+using ul = unsigned long;
+#define INF 999999999
+#define PRIME 1000000007
+int dy[]={0, 0, 1, -1};
+int dx[]={1, -1, 0, 0};
+#define REP(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
+#define ALL(x) (x).begin(),(x).end()
+int gcd(int a,int b){return b?gcd(b,a%b):a;}
+
+int main(){
+	cin.tie(0);
+  ios::sync_with_stdio(false);
+  int n,m;
+
+
+
+  return 0;
+}
