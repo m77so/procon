7ffe0822213dca3d9ca12bb5ee63ea7518f23fde
m77so <m77code@gmail.com>
2018-06-13 17:36:41 +0900

    バージョン表記の修正

--- templates/nim/a.nim
+++ templates/nim/a.nim
@@ -6,7 +6,7 @@ macro unpack*(rhs: seq,cnt: static[int]):auto  =
   let t = genSym();result = quote do:(let `t` = `rhs`;())
   when NimMajor == 0 and NimMinor < 17:
     for i in 0..<cnt:result[0][1].add(quote do:`t`[`i`])
-  when NimMajor >= 0 and NimMinor >= 17:
+  else:
     for i in 0..<cnt:result[1].add(quote do:`t`[`i`])
 
 template readInt(): int=
