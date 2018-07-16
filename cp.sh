d66e35b2070bb1e4671cf01312d1708b9f915c8f
m77so <m77code@gmail.com>
2018-07-16 23:31:18 +0900

    fix space

--- cp.sh
+++ cp.sh
@@ -9,7 +9,7 @@ else
 fi
 ext=${1#*.}
 if [ ${ext} = "cpp" ]; then
-  cp　-i ${shdir}/templates/cpp/a.cpp $1
+  cp -i ${shdir}/templates/cpp/a.cpp $1
 elif [ ${ext} = "nim" ]; then
   cp -i ${shdir}/templates/nim/a.nim $1
 elif [ ${ext} = "py" ]; then
